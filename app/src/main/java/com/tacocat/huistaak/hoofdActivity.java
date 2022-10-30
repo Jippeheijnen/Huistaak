98c157cb06f94e2e598a6d5e193f7a3d7168d4ca
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2022-10-30 20:31:42 +0100

    Move the server logic to the taakActivity

--- app/src/main/java/com/tacocat/huistaak/hoofdActivity.java
+++ app/src/main/java/com/tacocat/huistaak/hoofdActivity.java
@@ -12,11 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
+import java.util.stream.Collectors;
 
 public class hoofdActivity extends AppCompatActivity {
 
@@ -54,33 +58,6 @@ public class hoofdActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // trying the server shit
-                // and aparrently android doesn't like to have network stuff on the main thread lol
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try  {
-                            // url with the unique roster id
-                            URL url = new URL("https://testing.tacocat.site/test.txt");
-                            URLConnection conn = url.openConnection();
-
-                            HttpURLConnection httpConn = (HttpURLConnection) conn;
-                            httpConn.setAllowUserInteraction(false);
-                            httpConn.setInstanceFollowRedirects(true);
-                            httpConn.setRequestMethod("GET");
-                            httpConn.connect();
-
-                            InputStream is = httpConn.getInputStream();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-                thread.start();
-
-
 
                 Intent intent = new Intent(getApplicationContext(), taakActivity.class);
                 startActivity(intent);
