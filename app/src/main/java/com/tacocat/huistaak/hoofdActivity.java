d4a5966e47e1ada20b3c644ff0bb335e265d494d
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2022-10-30 18:11:08 +0100

    update

--- app/src/main/java/com/tacocat/huistaak/hoofdActivity.java
+++ app/src/main/java/com/tacocat/huistaak/hoofdActivity.java
@@ -1,9 +1,13 @@
 package com.tacocat.huistaak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,9 +21,33 @@ import java.util.Calendar;
 public class hoofdActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                //set the event you want to perform when button is clicked
+                //you can go to another activity in your app by creating Intent
+                Intent intent = new Intent(getApplicationContext(), InstellingenActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoofd);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
 
         Button button = (Button) findViewById(R.id.button);
 
@@ -28,30 +56,37 @@ public class hoofdActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 // trying the server shit
-                try {
+                // and aparrently android doesn't like to have network stuff on the main thread lol
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try  {
+                            // url with the unique roster id
+                            URL url = new URL("https://testing.tacocat.site/test.txt");
+                            URLConnection conn = url.openConnection();
 
-                    // url with the unique roster id
-                    URL url = new URL("");
-                    URLConnection conn = url.openConnection();
+                            HttpURLConnection httpConn = (HttpURLConnection) conn;
+                            httpConn.setAllowUserInteraction(false);
+                            httpConn.setInstanceFollowRedirects(true);
+                            httpConn.setRequestMethod("GET");
+                            httpConn.connect();
 
-                    HttpURLConnection httpConn = (HttpURLConnection) conn;
-                    httpConn.setAllowUserInteraction(false);
-                    httpConn.setInstanceFollowRedirects(true);
-                    httpConn.setRequestMethod("GET");
-                    httpConn.connect();
+                            InputStream is = httpConn.getInputStream();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
 
-                    InputStream is = httpConn.getInputStream();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                thread.start();
 
 
-                //set the event you want to perform when button is clicked
-                //you can go to another activity in your app by creating Intent
-                Intent intent = new Intent(getApplicationContext(), InstellingenActivity.class);
+
+                Intent intent = new Intent(getApplicationContext(), taakActivity.class);
                 startActivity(intent);
+
             }
+
         });
 
         int currentWeek = Calendar.getInstance().get(Calendar.WEEK_OF_YEAR);
--- app/src/main/java/com/tacocat/huistaak/taakActivity.java
+++ app/src/main/java/com/tacocat/huistaak/taakActivity.java
@@ -1,8 +1,16 @@
 package com.tacocat.huistaak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.prefs.Preferences;
 
 public class taakActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class taakActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taak);
+
+        TextView taakTextView = (TextView)findViewById(R.id.taakTextView);
+
+        // get current week and the according task to the selected user.
+
+        int currentWeek = Calendar.getInstance().get(Calendar.WEEK_OF_YEAR);
+        TextView weekNTextView = (TextView)findViewById(R.id.weekNTextView);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = preferences.getString("name", ":("); //"" is the default String to return if the preference isn't found
+
+        String result = username +
+                " moet in week " +
+                currentWeek +
+                " heel veel doen!";
+        taakTextView.setText(result);
+
     }
 }
